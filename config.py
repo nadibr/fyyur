3fa85a0fbb9faa456f098a97b70c865eb735262f
nadez <brusovanadez2010@students.itu.edu>
2020-09-17 19:50:28 -0700

    initial commit

--- config.py
+++ config.py
@@ -7,7 +7,4 @@ basedir = os.path.abspath(os.path.dirname(__file__))
 DEBUG = True
 
 # Connect to the database
-
-
-# TODO IMPLEMENT DATABASE URL
-SQLALCHEMY_DATABASE_URI = 'postgres://postgres:aRschloch1@localhost:5432/fyyur'
+SQLALCHEMY_DATABASE_URI = 'postgres://postgres:password@localhost:5432/fyyur'
